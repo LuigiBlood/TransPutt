6e8019ba2dd3eb793dee76db099575ea74070794
LuigiBlood <luigiblood@gmail.com>
2020-11-21 15:06:09 +0100

    Some design changes

--- TransPutt/Games/MarvelousForm.Designer.cs
+++ TransPutt/Games/MarvelousForm.Designer.cs
@@ -53,6 +53,7 @@
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.nextIDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownID1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview1)).BeginInit();
             this.panel1.SuspendLayout();
@@ -65,7 +66,7 @@
             // 
             this.comboBoxLang1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxLang1.FormattingEnabled = true;
-            this.comboBoxLang1.Location = new System.Drawing.Point(12, 48);
+            this.comboBoxLang1.Location = new System.Drawing.Point(12, 56);
             this.comboBoxLang1.Name = "comboBoxLang1";
             this.comboBoxLang1.Size = new System.Drawing.Size(262, 21);
             this.comboBoxLang1.TabIndex = 0;
@@ -74,7 +75,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 93);
+            this.label1.Location = new System.Drawing.Point(12, 90);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 1;
@@ -82,7 +83,7 @@
             // 
             // numericUpDownID1
             // 
-            this.numericUpDownID1.Location = new System.Drawing.Point(362, 49);
+            this.numericUpDownID1.Location = new System.Drawing.Point(362, 57);
             this.numericUpDownID1.Name = "numericUpDownID1";
             this.numericUpDownID1.Size = new System.Drawing.Size(51, 20);
             this.numericUpDownID1.TabIndex = 2;
@@ -101,16 +102,16 @@
             // 
             this.buttonSave1.Location = new System.Drawing.Point(12, 390);
             this.buttonSave1.Name = "buttonSave1";
-            this.buttonSave1.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave1.Size = new System.Drawing.Size(99, 23);
             this.buttonSave1.TabIndex = 4;
-            this.buttonSave1.Text = "Save";
+            this.buttonSave1.Text = "Save To Memory";
             this.buttonSave1.UseVisualStyleBackColor = true;
             this.buttonSave1.Click += new System.EventHandler(this.buttonSave1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 166);
+            this.label2.Location = new System.Drawing.Point(12, 163);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(31, 13);
             this.label2.TabIndex = 5;
@@ -137,7 +138,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 28);
+            this.label3.Location = new System.Drawing.Point(12, 36);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 13);
             this.label3.TabIndex = 8;
@@ -146,7 +147,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(305, 51);
+            this.label4.Location = new System.Drawing.Point(305, 59);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(51, 13);
             this.label4.TabIndex = 9;
@@ -163,11 +164,11 @@
             // 
             // buttonSaveAll1
             // 
-            this.buttonSaveAll1.Location = new System.Drawing.Point(137, 390);
+            this.buttonSaveAll1.Location = new System.Drawing.Point(159, 390);
             this.buttonSaveAll1.Name = "buttonSaveAll1";
             this.buttonSaveAll1.Size = new System.Drawing.Size(75, 23);
             this.buttonSaveAll1.TabIndex = 11;
-            this.buttonSaveAll1.Text = "Save to File";
+            this.buttonSaveAll1.Text = "Save To File";
             this.buttonSaveAll1.UseVisualStyleBackColor = true;
             this.buttonSaveAll1.Click += new System.EventHandler(this.buttonSaveAll1_Click);
             // 
@@ -191,7 +192,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(594, 28);
+            this.label5.Location = new System.Drawing.Point(594, 36);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(116, 13);
             this.label5.TabIndex = 17;
@@ -210,7 +211,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(455, 166);
+            this.label6.Location = new System.Drawing.Point(455, 163);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(31, 13);
             this.label6.TabIndex = 15;
@@ -228,7 +229,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(455, 93);
+            this.label7.Location = new System.Drawing.Point(455, 90);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(38, 13);
             this.label7.TabIndex = 13;
@@ -238,7 +239,7 @@
             // 
             this.comboBoxLang2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxLang2.FormattingEnabled = true;
-            this.comboBoxLang2.Location = new System.Drawing.Point(594, 48);
+            this.comboBoxLang2.Location = new System.Drawing.Point(594, 56);
             this.comboBoxLang2.Name = "comboBoxLang2";
             this.comboBoxLang2.Size = new System.Drawing.Size(262, 21);
             this.comboBoxLang2.TabIndex = 12;
@@ -252,7 +253,7 @@
             "Normal Text",
             "In-Game Text (2 Lines)",
             "Item Text (1 Line)"});
-            this.comboBoxStyle1.Location = new System.Drawing.Point(455, 48);
+            this.comboBoxStyle1.Location = new System.Drawing.Point(455, 57);
             this.comboBoxStyle1.Name = "comboBoxStyle1";
             this.comboBoxStyle1.Size = new System.Drawing.Size(121, 21);
             this.comboBoxStyle1.TabIndex = 18;
@@ -293,11 +294,21 @@
             this.nextIDToolStripMenuItem.Text = "Next ID";
             this.nextIDToolStripMenuItem.Click += new System.EventHandler(this.nextIDToolStripMenuItem_Click);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(455, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Style:";
+            // 
             // MarvelousForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(870, 422);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.comboBoxStyle1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBoxText2);
@@ -362,5 +373,6 @@
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToFileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem nextIDToolStripMenuItem;
+        private System.Windows.Forms.Label label8;
     }
 }
